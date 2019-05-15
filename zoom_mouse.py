f386da6bc92f0e6145dcc1e72f96cdb0f24201a1
Shean Lin <shean.lin2018@gmail.com>
2019-05-15 10:17:45 -0500

    a

---
+++ zoom_mouse.py
@@ -0,0 +1,5 @@
+# Custom eye mouse settings
+from talon_plugins import eye_mouse
+
+# eye_mouse.config.velocity = eye_mouse.Point2d(500, 500)
+eye_mouse.config.velocity = eye_mouse.Point2d(250, 700)
